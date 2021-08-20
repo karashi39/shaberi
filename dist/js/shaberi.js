6def4681dfa885f671308e4b2f69562367c27352
karashi39 <burrito.dr.peper@gmail.com>
2021-08-20 18:15:37 +0900

    refactor on client

--- dist/js/shaberi.js
+++ dist/js/shaberi.js
@@ -3,128 +3,110 @@ var uri = "ws://localhost:9999";
 var webSocket = null;
 
 function init() {
-    $("[data-name='message']").keypress(press);
-    open();
+  $("[data-name='message']").keypress(press);
+  open();
 }
 
 function open() {
-    if (webSocket == null) {
-        webSocket = new WebSocket(uri);
-        webSocket.onopen = onOpen;
-        webSocket.onmessage = onMessage;
-        webSocket.onclose = onClose;
-        webSocket.onerror = onError;
-    }
+  if (webSocket == null) {
+    webSocket = new WebSocket(uri);
+    webSocket.onopen = onOpen;
+    webSocket.onmessage = onMessage;
+    webSocket.onclose = onClose;
+    webSocket.onerror = onError;
+  }
 }
 
-function onOpen(event) {
-    chat("接続しました。");
+function onOpen() {
+  chat("接続しました。");
 }
 
 function onMessage(event) {
-    if (event && event.data) {
-        message = parseMsg(event.data);
-        chat(message);
-    }
+  if (event && event.data) {
+    const data = JSON.parse(event.data);
+    console.log(data);
+    if (data.msg_type === 'member_list') member_list(data);
+    if (data.msg_type === 'chat') chat(parseChat(data));
+  }
 }
 
 function onError(event) {
-    //chat("エラーが発生しました。");
+  console.warn(event);
+  chat("エラーが発生しました。");
 }
 
 function onClose(event) {
-    chat("切断しました。3秒後に再接続します。(" + event.code + ")");
-    webSocket = null;
-    setTimeout("open()", 3000);
+  chat("切断しました。3秒後に再接続します。(" + event.code + ")");
+  webSocket = null;
+  setTimeout("open()", 3000);
 }
 
 function press(event) {
-    if (event && event.which == 13) {
-        send();
-    }
+  if (event && event.which == 13) {
+    send();
+  }
 }
 
-function parseMsg(message){
-    // メッセージ表示
-    console.log(message);
-    try {
-        var msg = JSON.parse(message);
-    } catch(e){
-        console.log('unparsable json message.');
-        colour = 'red';
-    }
-    try {
-        colour = decodeURIComponent(escape(msg['colour']));
-    } catch (e) {
-        if(!colour){
-            colour = 'black';
-        }
-    }
-    colour = '<span style="color:' + colour + '">'
-    try {
-        hname = decodeURIComponent(escape(msg['hname']));
-    } catch (e){
-        hname = '';
-    }
-    if (hname) { hname = hname + ': '; }
-    try {
-        message = msg['message'];
-        message = decodeURIComponent(escape(message));
-    } catch (e) {
-    }
-    if (msg['msg_type'] == 'member_list') {
-        console.log(message);
-        member_list(message);
-        return;
-        //return '<span style="color:red">誰かの名前が変わりました</span>';
-    }
-    message = colour + hname + message + '</span>';
-    console.log(message);
-    return message
+function encode(text) {
+  return decodeURIComponent(text);
+}
+
+function decode(text) {
+  let ret = '';
+  try {
+    ret = decodeURIComponent(escape(text));
+  } catch (e) {
+    return text;
+  }
+  return ret;
+}
+
+function parseChat(data){
+  let color = decode(data.colour);
+  if(!color) color = 'black';
+  let hname = decode(data.hname);
+  let message = decode(data.message);
+  let span = `<span style="color: ${color};">${hname}: ${message}</span>`
+  return span;
 }
 
 function send(){
-    var sendmsg = {};
-    sendmsg.message = $("[data-name='message']").val();
-    sendmsg.hname = $("[data-name='hname']").val();
-    sendmsg.colour = $("[data-name='colour']").val();
-    // 存在チェック
-    if (sendmsg.message && webSocket) {
-         // メッセージ送信
-        sendmsg.message = decodeURIComponent(sendmsg.message);
-        sendmsg.hname = decodeURIComponent(sendmsg.hname);
-        sendmsg = JSON.stringify(sendmsg);
-        webSocket.send(sendmsg);
-        // メッセージ初期化
-        $("[data-name='message']").val("");
-    }
+  if (!webSocket) return;
+  const data = {
+    message: encode($("[data-name='message']").val()),
+    hname: encode($("[data-name='hname']").val()),
+    colour: encode($("[data-name='colour']").val()),
+  };
+  webSocket.send(JSON.stringify(data));
+  $("[data-name='message']").val("");
 }
 
-// チャットに表示
+// add chat row
 function chat(message) {
-    // 100件まで残す
-    var chats = $("[data-name='chat']").find("div");
-    while (chats.length >= 100) {
-        chats = chats.last().remove();
-    }
-    var msgtag = $("<div>").html(message);
-    $("[data-name='chat']").prepend(msgtag);
+  // 100件まで残す
+  var chats = $("[data-name='chat']").find("div");
+  while (chats.length >= 100) {
+    chats = chats.last().remove();
+  }
+  var msgtag = $("<div>").html(message);
+  $("[data-name='chat']").prepend(msgtag);
 }
 
-// チャットに表示
-function member_list(message) {
-    // 100件まで残す
-    console.log('member name changed');
-    $("[data-name='member-list']").html(message);
-    $(".info").find("button").addClass("pure-button");
-    $(".info").find("button").addClass("button-xsmall");
-    $(".info").find("button").addClass("pure-button-disabled");
+// update member list
+function member_list(data) {
+  console.log('member name changed');
+  $("[data-name='member-list']").html(data.message);
+  $(".info").find("button").addClass("pure-button");
+  $(".info").find("button").addClass("button-xsmall");
+  $(".info").find("button").addClass("pure-button-disabled");
 }
+
+// used by index.html
 function changeColor() {
-    var e = document.getElementById("colour-select");
-    var color = e.options[e.selectedIndex].style.color;
-    e.style.color = color;
-    $("#color-selected").value(color);
+  var e = document.getElementById("colour-select");
+  var color = e.options[e.selectedIndex].style.color;
+  e.style.color = color;
+  $("#color-selected").val(color);
 }
 
 // 初期処理登録
